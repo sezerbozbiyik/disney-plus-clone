e27ad9cf9a5352129fe9fe2c5c0e154a8489c66b
sezerbozbiyik <sezer.bozbiyik@icloud.com>
2021-09-06 23:58:43 +0300

    Add Slider

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Header from './component/Header'
+import Header from './component/Header';
 import Home from './component/Home';
 
 function App() {
