bd79683cd2b5ff2ab62b7d3adf74367638f026a3
sezerbozbiyik <sezer.bozbiyik@icloud.com>
2021-09-07 14:34:34 +0300

    add detail page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
-import './App.css';
-import Header from './component/Header';
-import Home from './component/Home';
+import React from "react";
+import "./App.css";
+import Header from "./component/Header";
+import Home from "./component/Home";
+import Detail from "./component/Detail";
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
-      <Home></Home>
+      <Router>
+        <Header/>
+        <Switch>
+          <Route path="/detail">
+            <Detail/>
+          </Route>
+          <Route path="/">
+            <Home/>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
