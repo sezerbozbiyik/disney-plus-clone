a7ef2bc088541d6b888ff1f2153e8d07f474650e
sezerbozbiyik <sezer.bozbiyik@icloud.com>
2021-09-08 21:25:58 +0300

    updated login page and route

--- src/component/Detail.js
+++ src/component/Detail.js
@@ -1,37 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
+import { useParams, useHistory } from "react-router-dom";
+import db from "../firebase";
 
 function Detail() {
+  const { id } = useParams();
+  const history = useHistory();
+  const [movie, setMovie] = useState();
+
+  useEffect(() => {
+    db.collection("movies")
+      .doc(id)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          setMovie(doc.data());
+        } else {
+          history.push("/");
+        }
+      });
+  }, []);
+  console.log(movie);
+
   return (
     <Container>
-      <Background>
-        <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/4F39B7E16726ECF419DD7C49E011DD95099AA20A962B0B10AA1881A70661CE45/scale?width=1440&aspectRatio=1.78&format=jpeg"></img>
-      </Background>
-      <ImageTitle>
-        <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/D7AEE1F05D10FC37C873176AAA26F777FC1B71E7A6563F36C6B1B497CAB1CEC2/scale?width=1440&aspectRatio=1.78"></img>
-      </ImageTitle>
-      <Controls>
-        <PlayButton>
-          <img src="/images/play-icon-black.png"></img>
-          <span>PLAY</span>
-        </PlayButton>
-        <TrailButton>
-          <img src="/images/play-icon-white.png"></img>
-          <span>TRAILER</span>
-        </TrailButton>
-        <AddButton>
-          <span>+</span>
-        </AddButton>
-        <GroupWatchButton>
-          <img src="/images/group-icon.png"></img>
-        </GroupWatchButton>
-      </Controls>
-      <SubTitle>2018 • 7m • Family, Fantasy, Kids, Animation</SubTitle>
-      <Description>
-        A Chinese mom who’s sad when her grown son leaves home gets another
-        chance at motherhood when one of her dumplings springs to life. But she
-        finds that nothing stays cute and small forever.
-      </Description>
+      {movie && (
+        <>
+          <Background>
+            <img src={movie.backgroundImg}></img>
+          </Background>
+          <ImageTitle>
+            <img src={movie.titleImg}></img>
+          </ImageTitle>
+          <Controls>
+            <PlayButton>
+              <img src="/images/play-icon-black.png"></img>
+              <span>PLAY</span>
+            </PlayButton>
+            <TrailButton>
+              <img src="/images/play-icon-white.png"></img>
+              <span>TRAILER</span>
+            </TrailButton>
+            <AddButton>
+              <span>+</span>
+            </AddButton>
+            <GroupWatchButton>
+              <img src="/images/group-icon.png"></img>
+            </GroupWatchButton>
+          </Controls>
+          <SubTitle>{movie.subTitle}</SubTitle>
+          <Description>{movie.description}</Description>
+        </>
+      )}
     </Container>
   );
 }
@@ -64,7 +84,8 @@ const ImageTitle = styled.div`
   min-height: 170px;
   width: 35vw;
   min-width: 200px;
-  margin-top:50px;
+  margin-top: 50px;
+  margin-bottom: 20px;
 
   img {
     width: 100%;
@@ -123,16 +144,16 @@ const GroupWatchButton = styled(AddButton)`
 `;
 
 const SubTitle = styled.div`
-    color: rgb(249, 249, 249);
-    font-size: 15px;
-    min-height: 20px;
-    margin-top: 26px;
-`
+  color: rgb(249, 249, 249);
+  font-size: 15px;
+  min-height: 20px;
+  margin-top: 26px;
+`;
 
 const Description = styled.div`
-    line-height: 1.4;
-    font-size: 20px;
-    margin-top: 16px;
-    color: rgb(249, 249, 249);
-    max-width: 760px;
-`
+  line-height: 1.4;
+  font-size: 20px;
+  margin-top: 16px;
+  color: rgb(249, 249, 249);
+  max-width: 760px;
+`;
