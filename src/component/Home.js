fd6f683c25475501917e2da1fc012afccefb9ba4
sezerbozbiyik <sezer.bozbiyik@icloud.com>
2021-09-08 19:03:32 +0300

    add grap movies from firebase db

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 import ImgSlider from "./ImgSlider";
 import Viewers from "./Viewers";
 import Movies from "./Movies";
 
+import db from "../firebase";
+import { useDispatch } from "react-redux";
+import { setMovies } from "../features/movie/movieSlice";
+
 function Home() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    db.collection("movies").onSnapshot((snapshot) => {
+      let tempMovies = snapshot.docs.map((doc) => {
+        return { id: doc.id, ...doc.data() };
+      });
+      dispatch(setMovies(tempMovies));
+    });
+  }, []);
+
   return (
     <Container>
       <ImgSlider></ImgSlider>
@@ -19,12 +34,12 @@ export default Home;
 const Container = styled.main`
   min-height: calc(100vh - 70px);
   padding: 0 calc(3.5vw + 5px);
-  position:relative;
-  overflow-x:hidden;
+  position: relative;
+  overflow-x: hidden;
 
   &:before {
-    background: url("/images/home-background.png") center center / cover 
-    no-repeat fixed;
+    background: url("/images/home-background.png") center center / cover
+      no-repeat fixed;
     content: "";
     position: absolute;
     top: 0;
